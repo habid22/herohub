d54a0c48f53963230c9bfea0bc69b2ee5284f5e1
habid22 <habid22@uwo.ca>
2023-11-16 18:23:08 -0500

    Add protected route and dashboard component

--- src/lib/routes.js
+++ src/lib/routes.js
@@ -2,6 +2,8 @@ import { createBrowserRouter } from "react-router-dom";
 
 import Login from "../components/auth/Login";
 import Register from "../components/auth/Register";
+import Dashboard from "..//components/dashboard/index";
+import Layout from "../components/layout";
 
 
 
@@ -13,10 +15,19 @@ export const LOGIN = "/login";
 export const REGISTER = "/register";
 
 
+export const PROTECTED = "/protected";
+export const DASHBOARD = "/protected/dashboard";
+
+
 export const router = createBrowserRouter([
     { path: ROOT, element: "Public Root" },
     { path: LOGIN, element: <Login /> },
     { path: REGISTER, element: <Register /> },
+    {path: PROTECTED, element: <Layout/>, children: [
+        { 
+            path: DASHBOARD, 
+            element: "Dashboard", },
+    ]},
 ]);
 
 
