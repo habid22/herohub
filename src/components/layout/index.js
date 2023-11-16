ded13bb70aa496551d7e847d8f9953562226f308
habid22 <habid22@uwo.ca>
2023-11-16 18:30:29 -0500

    Add firebase dependency and implement protected route redirection

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -1,8 +1,18 @@
-import { Outlet } from "react-router-dom";
+import { useEffect } from "react";
+import { Outlet, useLocation, useNavigate } from "react-router-dom";
+import { LOGIN } from "../../lib/routes";
 
 
 export default function Layout() {
-  return (
+    const { pathname } = useLocation();
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (pathname.startsWith("/protected")) {
+            navigate(LOGIN) 
+        }
+    }, [pathname]);
+    return (
    <>
    This is the child: <Outlet />
    
