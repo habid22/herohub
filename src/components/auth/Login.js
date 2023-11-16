b88bdffa034fd8a54a58c50f7f35dc5ad2af082f
habid22 <habid22@uwo.ca>
2023-11-16 18:13:22 -0500

    Refactor login component UI

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,9 +1,65 @@
-import React from 'react'
+import { Box, Center, FormLabel, Heading, Input, FormControl, FormErrorMessage, Button, Text, Link } from "@chakra-ui/react";
+import { REGISTER } from "../../lib/routes";
+import { Link as RouterLink } from "react-router-dom";
+
+
+
 
 export default function Login() {
+
   return (
-    <div>
-      Login
-    </div>
-  )
+    <Center w="100%" h="100vh">
+      <Box mx="1" maxW="md" p="9" borderWidth="1px" borderRadius="lg">
+        <Heading mb="4" size="lg" textAlign="center">
+          Log In
+        </Heading>
+
+        <form onSubmit={() => {}}>
+          <FormControl isInvalid={true} py="2">
+            <FormLabel>Email</FormLabel>
+            <Input type="email" placeholder="Enter your email here..." />
+            <FormErrorMessage>Invalid Email!</FormErrorMessage>
+          </FormControl>
+
+          <FormControl isInvalid={true} py="2">
+            <FormLabel>Password</FormLabel>
+            <Input type="password" placeholder="Enter your password here..." />
+            <FormErrorMessage>Invalid Password!</FormErrorMessage>
+          </FormControl>
+
+
+          <Button
+            mt="4"
+            type="submit"
+            colorScheme="teal"
+            size="md"
+            w="full"
+            isLoading={true}
+            loadingText="Logging In"
+          >
+            Log In
+          </Button>
+          </form>
+          <Text fontSize="xlg" align="center" mt="6">
+          Don't have an account?{" "}
+          <Link
+            as={RouterLink}
+            to={REGISTER}
+            color="teal.800"
+            fontWeight="medium"
+            textDecor="underline"
+            _hover={{ background: "teal.100" }}
+          >
+            Register here
+          </Link>{" "}
+          instead!
+        </Text>
+        
+        
+
+       
+      </Box>
+    </Center>
+  );
+  
 }
