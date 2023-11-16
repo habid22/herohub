c1645496921b9e42c8237bc1fee9d5154acc0cec
habid22 <habid22@uwo.ca>
2023-11-16 10:56:33 -0500

    Update import path for router in App.js and add routes to routes.js

--- src/lib/routes.js
+++ src/lib/routes.js
@@ -1,1 +1,10 @@
-export const router = {};
+import { createBrowserRouter } from "react-router-dom";
+
+export const ROOT = "/";
+
+
+export const router = createBrowserRouter([
+    { path: ROOT, element: "Public Root" },
+]);
+
+
