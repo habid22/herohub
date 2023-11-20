ba1468cbb49f4951edcf1e4baccc6e929e32bfa4
habid22 <habid22@uwo.ca>
2023-11-19 22:23:19 -0500

    Update package-lock.json and package.json, add search functionality to navbar

--- src/components/navbar/content.js
+++ src/components/navbar/content.js
@@ -18,7 +18,7 @@ import {
 import { MoonIcon, SunIcon } from "@chakra-ui/icons";
 import { Link as RouterLink } from "react-router-dom";
 import { LOGIN, REGISTER } from "../../lib/routes";
-import { FaSuperpowers, FaMoon, FaSun } from "react-icons/fa";
+import { FaSuperpowers, FaMoon, FaSun, FaSearch } from "react-icons/fa";
 
 export default function Content() {
   return (
@@ -107,6 +107,46 @@ function WelcomeContainer() {
 }
 
 function HeroSearchContainer() {
+  const [searchParams, setSearchParams] = useState({
+    name: "",
+    race: "",
+    publisher: "",
+    power: "",
+  });
+  const [searchResults, setSearchResults] = useState([]);
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setSearchParams((prevSearchParams) => ({
+      ...prevSearchParams,
+      [name]: value,
+    }));
+  };
+
+  const handleSearch = () => {
+    // Make a request to the server with search parameters
+    const apiUrl = `/api/superheroes/multi-search?name=${searchParams.name}&race=${searchParams.race}&publisher=${searchParams.publisher}&power=${searchParams.power}`;
+
+    fetch(apiUrl)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then((data) => {
+        if (!Array.isArray(data)) {
+          console.error('Invalid response format: expected an array', data);
+          throw new Error('Invalid response format');
+        }
+        setSearchResults(data);
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+        setSearchResults([]);
+      });
+  };
+
   return (
     <Flex
       mt={8}
@@ -124,15 +164,63 @@ function HeroSearchContainer() {
       </Text>
 
       <Stack spacing={4} align="center">
-        <Input placeholder="Enter a Name..." />
-        <Input placeholder="Enter a Race..." />
-        <Input placeholder="Enter a Publisher..." />
-        <Input placeholder="Enter a Power..." />
+        <Input
+          name="name"
+          placeholder="Enter a Name..."
+          value={searchParams.name}
+          onChange={handleInputChange}
+        />
+        <Input
+          name="race"
+          placeholder="Enter a Race..."
+          value={searchParams.race}
+          onChange={handleInputChange}
+        />
+        <Input
+          name="publisher"
+          placeholder="Enter a Publisher..."
+          value={searchParams.publisher}
+          onChange={handleInputChange}
+        />
+        <Input
+          name="power"
+          placeholder="Enter a Power..."
+          value={searchParams.power}
+          onChange={handleInputChange}
+        />
       </Stack>
 
-      <Button colorScheme="teal" mt={4}>
+      <Button colorScheme="teal" mt={4} onClick={handleSearch}>
+        <Icon as={FaSearch} boxSize={4} />
         Search for Hero
       </Button>
+
+      {/* Display search results */}
+      {searchResults.length > 0 && (
+        <Flex direction="column" mt={4}>
+          <Text fontSize="lg" fontWeight="bold" textAlign="center" mb={2}>
+            Search Results
+          </Text>
+          {searchResults.map((superhero, index) => (
+            <Flex
+              key={index}
+              border="1px solid teal"
+              borderRadius="md"
+              p={4}
+              mb={2}
+              alignItems="center"
+              justifyContent="space-between"
+            >
+              <Text>Name: {superhero.name}</Text>
+              <Text>Gender: {superhero.Gender}</Text>
+              <Text>Eye color: {superhero['Eye color']}</Text>
+              <Text>Race: {superhero.Race}</Text>
+              <Text>Hair color: {superhero['Hair color']}</Text>
+              {/* Add more properties as needed */}
+            </Flex>
+          ))}
+        </Flex>
+      )}
     </Flex>
   );
 }
