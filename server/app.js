4577034ceabf898ed1b0df4fb0f2fe58c93a7daf
habid22 <habid22@uwo.ca>
2023-11-21 20:29:08 -0500

    Fix typo in app.js

--- server/app.js
+++ server/app.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 4000;
-const fs = require('fs');z
+const fs = require('fs');
 
 // Use CORS middleware with origin option
 app.use(cors({ origin: 'http://localhost:3000' }));
